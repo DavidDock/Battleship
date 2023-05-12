c8d7784235b946f7f14ad7d0ced3ec3e734703f5
DavidDock <>
2023-05-12 13:23:11 +0000

    Add rules function to tell user game rules

--- run.py
+++ run.py
@@ -9,6 +9,19 @@ def welcome():
     print(" Welcome to BATTLESHIP\n")
 
 
+def rules(user):
+    """
+    describes rules for each game type
+    """
+    print(f" {user.name} you are at WAR! \n"
+          " Battle the enemy and sink their ships\n\n"
+          " Game 1 rules: \n"
+          " You have 12 shots to sink the enemy ships to win the war \n \n"
+          " Game 2 rules: \n"
+          " You and the enemy will take turns firing at each other's ships \n"
+          " First one to sink them all will win the war \n")
+
+
 class Player:
     """
     class for player with methods to
@@ -29,12 +42,11 @@ class Player:
         uses input to name player
         returns name
         """
-        print("Ok Captain what is your name? \n")
+        print(" Ok Captain what is your name? \n")
         user_name = input("\n")
         if user_name == "":
             user_name = "lazy bones"
         self.name = user_name.upper()
-        print(f"\nHello {self.name}\n")
         return self.name
 
 
@@ -45,6 +57,7 @@ def main():
     welcome()
     user = Player("", 0, 0)
     Player.get_user_name(user)
+    rules(user)
 
 
 main()
