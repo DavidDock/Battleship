17a99056435d96497a4ab0fe74980e795dcd2119
DavidDock <>
2023-05-12 20:18:04 +0000

    Create method for player to pick grid size an function to validate input

--- run.py
+++ run.py
@@ -13,13 +13,27 @@ def rules(user):
     """
     describes rules for each game type
     """
-    print(f" {user.name} you are at WAR! \n"
+    print(f"\n {user.name} you are at WAR! \n"
           " Battle the enemy and sink their ships\n\n"
           " Game 1 rules: \n"
           " You have 12 shots to sink the enemy ships to win the war \n \n"
           " Game 2 rules: \n"
           " You and the enemy will take turns firing at each other's ships \n"
           " First one to sink them all will win the war \n")
+
+
+def validate_data(data, data_list):
+    """
+    function to validate player input
+    """
+    try:
+        if data not in data_list:
+            raise ValueError(
+                f"Please choose : {' '.join(data_list)}")
+    except ValueError as e:
+        print(f" Invalid entry. {e}")
+        return False
+    return True
 
 
 class Player:
@@ -43,11 +57,32 @@ class Player:
         returns name
         """
         print(" Ok Captain what is your name? \n")
-        user_name = input("\n")
+        user_name = input(" \n")
         if user_name == "":
             user_name = "lazy bones"
         self.name = user_name.upper()
         return self.name
+
+    def size_choices(self):
+        """
+        Gets user input for grid size
+        validates input
+        uses input to alocate players size
+        returns size
+        """
+        grids = ["4", "5", "6", "7", "8", ""]
+        while True:
+            print(" Pick your battlefield grid size:\n")
+            choice = input(" 4 5 6 7 or 8? \n")
+            if validate_data(choice, grids):
+                break
+        if choice == "":
+            print("Ok The computer will pick for you\n")
+            self.size = randint(4, 8)
+            return self.size
+        print(f" You picked battlefield grid size {choice}\n")
+        self.size = int(choice)
+        return self.size
 
 
 def main():
@@ -58,6 +93,7 @@ def main():
     user = Player("", 0, 0)
     Player.get_user_name(user)
     rules(user)
+    Player.size_choices(user)
 
 
 main()
